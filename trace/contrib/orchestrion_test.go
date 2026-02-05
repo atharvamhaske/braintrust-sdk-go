0f248f2de669069d2361cc61fe4b817f97b69e6f
Aswin Karumbunathan <aswin@braintrustdata.com>
2026-02-05 15:24:57 -0800

    Pin orchestrion to 1.6.1
    That's the version we currently use, since 1.7.0 came out two weeks ago, CI
    will break unless we either pin the version or upgrade.

--- trace/contrib/orchestrion_test.go
+++ trace/contrib/orchestrion_test.go
@@ -23,7 +23,7 @@ func TestOrchestrionInjection(t *testing.T) {
 
 	// Check orchestrion is available
 	if _, err := exec.LookPath("orchestrion"); err != nil {
-		t.Fatal("orchestrion not found in PATH (install: go install github.com/DataDog/orchestrion@latest)")
+		t.Fatal("orchestrion not found in PATH (install: go install github.com/DataDog/orchestrion@1.6.1)")
 	}
 
 	// Get the testdata directory
